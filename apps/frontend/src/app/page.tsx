95f9ed8ec033f6bcbd97cb7794ff4277bbf1be79
João Vitor Veronese Vieira <joao.veronese@timeprimo.com>
2023-12-08 10:47:52 -0300

    feat: create Modal component (14)

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Card from '@/components/shared/Card';
+import Modal from '@/components/shared/Modal';
 
 export default function Home() {
   return (
     <div>
       <Card color='primary'> Estou dentro de um card </Card>
+
+      <Modal visible> Estou dentro do modal</Modal>
     </div>
   );
 }
